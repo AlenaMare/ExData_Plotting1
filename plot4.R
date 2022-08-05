eac833d541b67374bf4cec0c78fff6eb33182cc1
Alena Mare <71310698+AlenaMare@users.noreply.github.com>
2022-08-05 12:10:23 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ plot(subsetpower$TimeDate, subsetpower$Global_active_power, type = "l", xlab="",
 with(subsetpower, plot(subsetpower$TimeDate, subsetpower$Sub_metering_1, type = "l", xlab="", ylab="Energy sub metering"))
 lines(subsetpower$TimeDate, subsetpower$Sub_metering_2, type = "l", col = "red")
 lines(subsetpower$TimeDate, subsetpower$Sub_metering_3, type = "l", col = "blue")
-legend(c("topright"), c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=1, col = c("black","red","blue"))
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=1, col = c("black","red","blue"), bty = "n")
 plot(subsetpower$TimeDate, subsetpower$Voltage, type = "l", xlab="datetime", ylab="Voltage")
 plot(subsetpower$TimeDate, subsetpower$Global_reactive_power, type = "l", xlab="datetime", ylab="Global_reactive_power")
 dev.off() 
